d2ec69d3965fdad48749a136143021c6c82075e4
Blue00123 <101937156+Blue00123@users.noreply.github.com>
2022-06-18 03:47:00 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,35 +1,18 @@
 //made by Blue00123
 
-//cps 사용여부 (true/false)
-let usecpsactionbar: boolean = true;
-
-events.playerJoin.on((ev) => {
-    bedrockServer.executeCommand(`scoreboard objectives add cps dummy`);
-});
-
-events.packetBefore(MinecraftPacketIds.LevelSoundEvent).on((ev, ni) => {
-    const playerName = ni.getActor()?.getName();
-    if (ev.sound === 42) {
-            bedrockServer.executeCommand(`scoreboard players add ${playerName} cps 1`);
-        if (usecpsactionbar) {
-            bedrockServer.executeCommand(`titleraw @a actionbar {"rawtext":[{"text":"§l§fCPS:§e "},{"score":{"name":"*","objective":"cps"}}]},{"text":""}]}`);`);
+const CPS: any = {};
+events.packetBefore(MinecraftPacketIds.LevelSoundEvent).on((pkt, ni) => {
+        const playerName = ni.getActor()!.getName();
+        if (pkt.sound === 42 || pkt.sound === 43) {
+            if (!CPS[playerName]) {
+                CPS[playerName] = 0;
+            }
+            CPS[playerName]++;
+            bedrockServer.executeCommand(`title ${playerName} actionbar CPS :${CPS[playerName]}`);
+            setTimeout(() => {
+                CPS[playerName]--;
+                bedrockServer.executeCommand(`title ${playerName} actionbar CPS :${CPS[playerName]}`);
+            }, 1000);
         }
     }
 });
-
-events.playerAttack.on((ev) => {
-    const playerName = ev.player.getName();
-    bedrockServer.executeCommand(`scoreboard players add ${playerName} cps 1`);
-    if (usecpsactionbar) {
-        bedrockServer.executeCommand(`titleraw @a actionbar {"rawtext":[{"text":"§l§fCPS:§e "},{"score":{"name":"*","objective":"cps"}},{"text":""}]}`);
-    }
-    
-});
-
-const cool = setInterval(() => {
-    bedrockServer.executeCommand(`scoreboard players set @a cps 0`);
-},1000);
-
-events.serverClose.on(() => {
-    clearInterval(cool)
-})
